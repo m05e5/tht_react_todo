48499a222942808d694f0b6225f627a4b3ad7d96
m05e5 <calebmoses1111@gmail.com>
2022-03-13 20:39:33 +0100

    add methods to clear done todos, delete todos, update todos, and refactored

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -15,6 +15,24 @@ const Todo = () => {
     })
   } 
 
+  const submitTodo = () => {
+    const todoInput = document.getElementById('todo_input')
+    if(todoInput.value !== '') {
+      console.log(todoInput.value);
+        axios.post(baseURL, {
+          name: todoInput.value,
+          completed: false
+        }).then((response) => {
+          console.log(response);
+        }).then (() => {
+          fetchTodos();
+          todoInput.value = '';
+        })
+    } else {
+      console.log('baka');
+    }
+  }
+
   const deleteTodo = (id) => {
     axios.delete(`${baseURL}/${id}`)
     .then(() => {
@@ -22,10 +40,39 @@ const Todo = () => {
     })
   } 
 
-  // fetchTodos(() => {
-  // getTodos();
-  //   console.log(todos)
-  // }, [])
+  const clearTodo = () => {
+    todos.forEach((element)=>{
+      if(element.completed === true) {
+        deleteTodo(element.id);
+      }
+    })
+    fetchTodos()
+  }
+
+
+  const checkTodo = (todoId) => {
+    const todoCheck = document.getElementById(todoId)
+    console.log(todoCheck.checked)
+    if(todoCheck.checked === true) {
+      axios.patch(`${baseURL}/${todoId}`, {
+        completed: true,
+      })
+      .then(() => {
+        fetchTodos()
+      });
+    } else {
+        axios.patch(`${baseURL}/${todoId}`, {
+          completed: false,
+        })
+        .then(() => {
+          fetchTodos()
+        });
+    }
+
+  }
+  useEffect(() => {
+    fetchTodos()
+  }, []);
 
   const displayTodo = () => {
     if(todos.length !== 0) {
@@ -33,16 +80,20 @@ const Todo = () => {
       return (<>
         {
           todos.map((element) => (
-            <div className="todo">
+            <div key={element.id} className="todo">
               <label className="todo_check" htmlFor={element.id}>
                 <div>
-                  <input type="checkbox" id={element.id}/>
+                  <input className="todo_check" onClick={() => checkTodo(element.id)} type="checkbox" id={element.id}/>
                   <span className="checkmark" />
-                  <b className="todo_name"> {element.name} </b>
+                  <b 
+                    className={`todo_name ${element.completed ? 'completed' : null}`}
+                  >
+                    {element.name} 
+                  </b>
                 </div>
-                <div className="deleteTodo" onClick={() => deleteTodo(element.id)}>
-                 <b> X</b>
-                </div>
+                <button className="deleteTodo" onClick={() => deleteTodo(element.id)}>
+                  X
+                </button>
               </label>
             </div>
           ))
@@ -57,12 +108,22 @@ const Todo = () => {
   return (
     <div className="todo_div">
       <div className="todo_header">
-          <input type="text" placeholder='Task name:' className='box_in' />
-          <button type="button" className='box_in' onClick={fetchTodos}><b>ADD TODO</b></button>
+          <input type="text" placeholder='Task name:' id="todo_input" className=' box_in' />
+          <button type="button" className='box_in' onClick={submitTodo}><b>ADD TODO</b></button>
       </div>
       <div className="todo_body box_in">
-        <div>
-
+        <div className="clear_done_div">
+          <div className='count'>
+            <b>
+              1
+              /
+              {' '}
+              {todos.length}
+            </b>
+          </div>
+          <button className="clear_done" onClick={clearTodo}>
+           X CLEAR DONE TODOS
+          </button>
         </div>
         <div className='todoList'>
           {displayTodo()}
