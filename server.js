1c7f786569049d0ea74a80dafbed0fffefa55a97
m05e5 <calebmoses1111@gmail.com>
2022-03-14 10:00:32 +0100

    fixing server

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-// const path = require('path');
-// const express = require('express');
-const middlewares = jsonServer.defaults({
-  static: "./build"
-});
-const router = jsonServer.router('./db.json');
-const port = process.env.PORT || 3001;
+const path = require('path');
+const express = require('express');
 
-//----
-server.use(middlewares)
-//----
-
-
-// server.use('/db', middlewares, router);
-// server.use(express.static(path.join(__dirname, 'build')));
-
-//----
-server.use(jsonServer.rewriter({
-  "/api/*": "$1",
-}))
-//----
-
-// server.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+const middlewares = jsonServer.defaults()
+//-----
+// const middlewares = jsonServer.defaults({
+//   static: "./build"
 // });
+//-----
+
+const router = jsonServer.router('./db.json');
+const port = process.env.PORT || 4000;
+
+//----
+// server.use(middlewares)
+//----
+
+
+server.use('/db', middlewares, router);
+server.use(express.static(path.join(__dirname, 'build')));
+
+//----
+// server.use(jsonServer.rewriter({
+//   "/api/*": "$1",
+// }))
+//----
+
+server.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 //----
-server.use(router)
+// server.use(router)
 //----
 
 
